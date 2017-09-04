968ff4c711c21333231b435433ba44b6761e273b
Pontus Laestadius <guslaepo@student.gu.se>
2017-09-04 09:41:27 +0200

    init

---
+++ target/debug/ftp-rust.d
@@ -0,0 +1,1 @@
+/home/pontus/RustProjects/ftp-rust/target/debug/ftp-rust: /home/pontus/RustProjects/ftp-rust/src/lib.rs /home/pontus/RustProjects/ftp-rust/src/main.rs
---
+++ target/debug/libftp_rust.d
@@ -0,0 +1,1 @@
+/home/pontus/RustProjects/ftp-rust/target/debug/libftp_rust.rlib: /home/pontus/RustProjects/ftp-rust/src/lib.rs

b50b5d4d9a032a8899ce7d705b7626c4a4b67d88
Pontus Laestadius <guslaepo@student.gu.se>
2017-09-04 09:43:41 +0200

    fix

--- target/debug/ftp-rust.d
+++
@@ -1,1 +0,0 @@
-/home/pontus/RustProjects/ftp-rust/target/debug/ftp-rust: /home/pontus/RustProjects/ftp-rust/src/lib.rs /home/pontus/RustProjects/ftp-rust/src/main.rs
--- target/debug/libftp_rust.d
+++
@@ -1,1 +0,0 @@
-/home/pontus/RustProjects/ftp-rust/target/debug/libftp_rust.rlib: /home/pontus/RustProjects/ftp-rust/src/lib.rs
